08216eb4d1c27f5bef745a3012b8f409d500e7c5
Abdul Muqeet <muqeeabd@corp.contoursoftware.net>
2022-12-18 19:44:48 +0500

    This is final change

--- test.js
+++ test.js
@@ -1,17 +1,25 @@
-// This is a function to check weather the string is palindrome 
- function thepalindrome(submit.value){
-        
-        if(submit.length >= 10){
-            document.getElementById('ans').innerHTML = "Extended lenght";
-        }
+const txtInput = document.querySelector(".inputs input"),
+checkBtn = document.querySelector(".inputs button"),
+infoTxt = document.querySelector(".info-txt");
+let filterInput;
 
+checkBtn.addEventListener("click", () => {
+    let reverseInput = filterInput.split("").reverse().join("");
+    infoTxt.style.display = "block";
+    if(filterInput != reverseInput) {
+        return infoTxt.innerHTML = `No, <span>'${txtInput.value}'</span> isn't a palindrome!`;
+    }
+    infoTxt.innerHTML = `Yes, <span>'${txtInput.value}'</span> is a palindrome!`;
+});
 
- }
-
-
-    
-
-
+txtInput.addEventListener("keyup", () => {
+    filterInput = txtInput.value.toLowerCase().replace(/[^A-Z0-9]/ig, "");
+    if(filterInput) {
+        return checkBtn.classList.add("active");
+    }
+    infoTxt.style.display = "none";
+    checkBtn.classList.remove("active");
+});
 
 
 
